beaad578eeddaf2c63e1a120dab243fc9ae5f317
Biscuit-elixr <vasquezben24@gmail.com>
2026-01-29 12:42:08 -0500

    Began Coding Tenna Destruction
    - Destroying and instantiating Tennas

--- Assets/Scripts/BarrierHealth.cs
+++ Assets/Scripts/BarrierHealth.cs
@@ -6,37 +6,77 @@ public class BarrierHealth : MonoBehaviour
     [SerializeField] private float health;
     public bool isDestroyed;
 
+    public GameObject tennaNormal;
+    public GameObject tennaDamaged_1;
+    public GameObject tennaDamaged_2;
+
+    public bool tennaNormal_isDestroyed = false;
+
     public ScreenShake shakeScreen;
+
+    public DestroyCreate destroyAndCreate;
 
     private void Start()
     {
         isDestroyed = false;
 
-        if (shakeScreen != null)
+        /*if (shakeScreen != null)
         {
             Debug.Log("Shake has occurred!");
         }
         else
         {
             Debug.Log("Shake didn't work.");
-        }
+        }*/
+
+        
 
     }
 
     public void TakeDamage(float damage)
     {
-        health -= damage;
-        Debug.Log(health);
 
-        if(health <= 0)
+        //Quaternion targetRotation = Quaternion.Euler(0f, 180f, 0f);
+
+        health -= damage;
+
+
+        if (health == 2)
+        {
+            destroyAndCreate.DestroyTennaNormal();
+            //Destroy(tennaNormal);
+
+            //tennaNormal_isDestroyed=true;
+
+            //Instantiate(tennaDamaged_1, new Vector3(32.71f, 5.92f, 20.05f), targetRotation).transform.localScale = new Vector3(171.9393f, 171.9393f, 171.9393f);
+
+            Debug.Log("Current Health " + health);
+            Debug.Log("Now showing Tenna Damage 1");
+        }
+        
+        if(health == 1)
+        {
+            //Destroy(tennaDamaged_1);
+
+            //Instantiate(tennaDamaged_2, new Vector3(32.71f, 5.92f, 20.05f), targetRotation).transform.localScale = new Vector3(171.9393f, 171.9393f, 171.9393f);
+
+            destroyAndCreate.DestroyDamagedTenna();
+
+            Debug.Log("Hits left " + health);
+            Debug.Log("Now showing Tenna Damage 2");
+        }
+
+        //Debug.Log(health);
+
+        if (health <= 0)
         {
             shakeScreen.BeginShaking();
 
             //isDestroyed = true;
             //Debug.Log("Is Wall Destoyed: "+ isDestroyed);
             Destroy(gameObject);
-            
-         
+
+
 
         }
     }
---
+++ Assets/Scripts/DestroyCreate.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class DestroyCreate : MonoBehaviour
+{
+
+    public GameObject tennaNormal;
+    public GameObject tennaDamaged_1;
+    public GameObject tennaDamaged_2;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DestroyTennaNormal()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0f, 180f, 0f);
+
+        Destroy(tennaNormal);
+
+        Instantiate(tennaDamaged_1, new Vector3(32.71f, 5.92f, 20.05f), targetRotation).transform.localScale = new Vector3(171.9393f, 171.9393f, 171.9393f);
+
+    }
+
+    public void DestroyDamagedTenna()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0f, 180f, 0f);
+
+        Destroy(tennaDamaged_1);
+
+        Instantiate(tennaDamaged_2, new Vector3(32.71f, 5.92f, 20.05f), targetRotation).transform.localScale = new Vector3(171.9393f, 171.9393f, 171.9393f);
+    }
+
+    public void DestroyMoreDamagedTenna()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0f, 180f, 0f);
+
+        Destroy(tennaDamaged_2);
+
+        //Instantiate(tennaDamaged_2, new Vector3(32.71f, 5.92f, 20.05f), targetRotation).transform.localScale = new Vector3(171.9393f, 171.9393f, 171.9393f);
+    }
+}
